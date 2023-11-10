c1daeaf14ba8b552570a0e23bd749d06d977b0ab
matternstsr <matternstsr@gmail.com>
2023-11-10 11:24:52 -0600

    n=n  passes the value of n to the template.

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,7 +34,7 @@ def isanumber(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def numbertemplate(n):
-    return render_template('5-number.html')
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
