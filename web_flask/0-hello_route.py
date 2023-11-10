09c747f201ad37fe40050de35292736a005d1e1c
matternstsr <matternstsr@gmail.com>
2023-11-10 08:52:57 -0600

    E302 expected 2 blank lines, found 0  E304 blank lines found after function decorator

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,9 +2,9 @@
 """Return Hello HBNB when started"""
 from flask import Flask
 app = Flask(__name__)
+
+
 @app.route('/', strict_slashes=False)
-
-
 def say_hello():
     return 'Hello HBNB!'
 if __name__ == '__main__':
