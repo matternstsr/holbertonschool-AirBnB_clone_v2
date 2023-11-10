31522d0a60dcbfe9befbfc8c7950ac7212d4fa07
matternstsr <matternstsr@gmail.com>
2023-11-10 08:48:24 -0600

    0-hello_route.py:5:1: E302 expected 2 blank lines, found 00-hello_route.py:7:1: W391 blank line at end of file added 0.0.0.0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,5 +3,9 @@
 from flask import Flask
 app = Flask(__name__)
 @app.route('/', strict_slashes=False)
+
+
 def say_hello():
     return 'Hello HBNB!'
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
