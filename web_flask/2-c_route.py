87345615c0bb5bad0886c24559fc87d224d28ba5
matternstsr <matternstsr@gmail.com>
2023-11-10 10:04:10 -0600

    2 works on pc lets test

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,10 +8,14 @@ app = Flask(__name__)
 def say_hello():
     return "Hello HBNB!"
 
-
 @app.route('/hbnb', strict_slashes=False)
 def say_hnb():
     return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisnotfun(text):
+    text = text.replace('_', ' ')
+    return "C{}".format(text)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
