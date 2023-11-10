f556e549b6ce46b24b840db0fcf5fab5eb57d752
matternstsr <matternstsr@gmail.com>
2023-11-10 09:47:17 -0600

    1.

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Display string depending on the root"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def say_hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def say_hnb():
+    return "HBNB"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
