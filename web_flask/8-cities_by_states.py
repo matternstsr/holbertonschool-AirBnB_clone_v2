7c7c9de5c470268672a08cf08477bd5fd51a9944
matternstsr <matternstsr@gmail.com>
2023-11-14 10:51:02 -0600

    still not accessing the storage

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """Return string when navigating to root dir"""
-from models.state import State
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -13,7 +12,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/cities_by_states', strict_slashes=False)
 def list_state_cities():
-    states = storage.all(State).values()
+    states = storage.all('State')
     return render_template('8-cities_by_states.html', states=states)
 
 @app.teardown_appcontext
