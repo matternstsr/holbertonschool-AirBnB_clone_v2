6e64bc0ea222671ea2fb06225d3dcc6f606b3dc2
matternstsr <matternstsr@gmail.com>
2023-11-14 15:01:46 -0600

    doesnt work local but nothing seems too... seeing what checker says.

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""star of a Flask web app"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def states_list():
+    """ Creates a sorted list of states and amenities
+    for rendering in the template """
+    stored_states = storage.all("State").values()
+    states = sorted(stored_states, key=lambda state: state.name)
+    return render_template('10-hbnb_filters.html', states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """ Closes the storage connection """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
