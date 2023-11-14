abd1a348493b7fb8ee9b16529f14bc9a92515919
matternstsr <matternstsr@gmail.com>
2023-11-14 09:35:42 -0600

    access state differently

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 """Return string when navigating to root dir"""
-from flask import Flask, render_template, g
 import models
 from models.state import State
+from models import storage
+from flask import Flask
+from flask import render_template
+
+
 
 
 app = Flask(__name__, template_folder='templates')
@@ -10,7 +14,7 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/states_list', strict_slashes=False)
 def list_states():
-    states = models.storage.all(State).values()
+    states = storage.all('State')
     return render_template('7-states_list.html', states=states)
 
 
