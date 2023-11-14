d0df7471f43176afa8f4f0e2e2f7f1ad3a6ad339
matternstsr <matternstsr@gmail.com>
2023-11-14 12:52:26 -0600

    web_flask/8-cities_by_states.py:24:1: W293 blank line contains whitespace  web_flask/8-cities_by_states.py:24:1: W391 blank line at end of file

---
+++ web_flask/9-states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Return string when navigating to root dir"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def list_state_cities():
+    states = storage.all('State')
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states/<id>')
+def state_cities(id):
+    state = storage.get("State", id)
+    if state:
+        cities = sorted(state.cities, key=lambda city: city.name)
+        return (render_template('9-states.html', state=state, cities=cities))
+    else:
+        return render_template('9-states.html', nf=True)
+
+
+@app.teardown_appcontext
+def tear_down(error):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
